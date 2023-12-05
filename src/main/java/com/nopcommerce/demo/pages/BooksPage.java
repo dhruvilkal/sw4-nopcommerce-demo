459044341fa41308041a4a3350e81aaa33bb67fd
dhruv <dhruvilkalathiya04@gmail.com>
2023-12-05 21:50:11 +0000

    My nop-commerce-sw-4

---
+++ src/main/java/com/nopcommerce/demo/pages/BooksPage.java
@@ -0,0 +1,16 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.utilities.Utility;
+import org.openqa.selenium.By;
+
+public class BooksPage extends Utility {
+    By welcomeText = By.xpath("//h1[normalize-space()='Books']");
+
+
+
+
+
+    public String getWelcomeText() {
+        return getTextFromElement(welcomeText);
+    }
+}
---
+++ src/test/java/com/nopcommerce/demo/testsuite/TopMenuTest.java
@@ -0,0 +1,33 @@
+package com.nopcommerce.demo.testsuite;
+
+import com.nopcommerce.demo.pages.*;
+import com.nopcommerce.demo.testbase.BaseTest;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TopMenuTest extends BaseTest {
+    HomePage homePage = new HomePage();
+    ComputersPage computersPage = new ComputersPage();
+    ElectronicsPage electronicsPage = new ElectronicsPage();
+    ApparelPage apparelPage = new ApparelPage();
+    DigitalDownloadsPage digitalDownloadsPage = new DigitalDownloadsPage();
+    BooksPage booksPage = new BooksPage();
+    JewelryPage jewelryPage = new JewelryPage();
+    GiftCardsPage giftCardsPage = new GiftCardsPage();
+
+
+    @Test
+    public void selectMenu() {
+        String menu = "Computers";
+
+         List<WebElement> list = homePage.getListOfTopMenu();
+        for (WebElement element: list) {
+            if(element.getText().trim().equalsIgnoreCase(menu)){
+                homePage.clickOnComputersMenu();
+                break;
+            }
+        }
+    }
+}
